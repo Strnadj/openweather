2c1a5a790e28e31f889fe657e8d67434de271e22
Jan Strnadek <jan.strnadek@eman.cz>
2018-02-26 13:22:37 +0100

    Add icon response

--- lib/openweather2/weather.rb
+++ lib/openweather2/weather.rb
@@ -1,8 +1,8 @@
 module Openweather2
   class Weather
-
     attr_reader :city, :longitude, :latitude, :pressure, :humidity, :clouds,
-      :temperature, :min_temperature, :max_temperature, :wind_speed, :wind_angle
+                :temperature, :min_temperature, :max_temperature, :wind_speed, :wind_angle,
+                :icon
 
     def initialize(json)
       @city = json['name']
@@ -16,6 +16,9 @@ module Openweather2
       @clouds = json['clouds']['all']
       @wind_speed = json['wind']['speed']
       @wind_angle = json['wind']['deg']
+      @icon = if json['weather'] &&  json['weather'][0]
+                @icon = json['weather'][0]['icon']
+              end
     end
   end
 end
